1350ee0a72b03860efdd7192267ab6217efbdb1d
Andy Friedman <afriedman412@gmail.com>
2023-08-10 22:23:23 -0400

    first upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template(f'page.html', tag='home')
+
+@app.route('/<tag>')
+def data(tag):
+   with open('entries.json') as json_file:
+      data = json.load(json_file)
+   return render_template(f'page.html', data=data, tag=tag)
+
+if __name__=="__main__":
+   app.run(debug=True)
