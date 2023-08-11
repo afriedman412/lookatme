906beabfebb3d415cf2fc79548b84a1a0760c918
Andy Friedman <afriedman412@gmail.com>
2023-08-11 12:14:21 -0400

    passenger update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,9 @@
 import sys, os
 cwd = os.getcwd()
 sys.path.append(cwd)
+INTERP = "/home/dnpdata/dnpdata.com/public/dnpdata/venv"
+
+#INTERP is present twice so that the new Python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
 from app import app as application
