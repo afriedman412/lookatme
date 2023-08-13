138d557bdd01ce8fa215417c69f0e2042f3d7c84
Andy Friedman <afriedman412@gmail.com>
2023-08-13 16:26:44 -0400

    entry path tweak

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template
 import json
+import os
 
 
 def home():
-   return render_template(f'page.html', tag='home')
+   return render_template('page.html', tag='home')
 
 def page(tag):
-   with open('./app/data/entries.json') as json_file:
+   print(os.getcwd())
+   with open('app/data/entries.json') as json_file:
       data = json.load(json_file)
-   return render_template(f'page.html', data=data, tag=tag)
+   return render_template('page.html', data=data, tag=tag)
